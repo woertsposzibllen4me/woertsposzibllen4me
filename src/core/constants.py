addce7775fc7bf7d50ce9ce321afee783bebf219
Loïc Villet <villet.loic@gmail.com>
2026-01-08 18:57:39 +0100

    style: Clarify source of ws url for streamerbot

--- src/core/constants.py
+++ src/core/constants.py
@@ -12,11 +12,12 @@ LOCK_FILES_DIR_PATH = TEMP_DIR_PATH / "lock_files"
 COMMON_LOGS_FILE_PATH = LOG_DIR_PATH / "all_logs.log"
 
 # URLs
-STREAMERBOT_WS_URL = "ws://127.0.0.1:50001/"
+STREAMERBOT_WS_URL = "ws://127.0.0.1:50001/"  # Defined in the Streamer.bot app settings
 
 # Subprocesses
 STOP_SUBPROCESS_MESSAGE = "stop$subprocess"  # $ character is used to avoid accidental
 # trigger from speech to text.
+
 SUBPROCESSES_PORTS = {
     # list of subprocesses name and their socket server ports
     "shopwatcher": 59000,

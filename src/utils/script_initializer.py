3552356849324b055d3c7f2e6e98d8202ce64024
Loïc Villet <villet.loic@gmail.com>
2026-01-09 00:15:06 +0100

    refactor: Make linters happy with script_initializer.py

--- src/utils/script_initializer.py
+++ src/utils/script_initializer.py
@@ -1,7 +1,10 @@
+"""Utility to initialize terminal window managed scripts."""
+
 import atexit
 import signal
 import sys
-from typing import Optional
+from collections.abc import Callable
+from types import FrameType
 
 import aiosqlite
 
@@ -9,6 +12,8 @@ from src.core.termwm import (
     TERMINAL_WINDOW_SLOTS_DB_FILE_PATH,
     TerminalWindowManager,
     WinType,
+)
+from src.core.termwm import (
     slots_db_handler as sdh,
 )
 from src.core.termwm.core.types import SecondaryWindow
@@ -19,73 +24,68 @@ from src.utils.logging_utils import setup_logger
 SCRIPT_NAME = construct_script_name(__file__)
 logger = setup_logger(SCRIPT_NAME)
 
-cleanup_functions = []
+cleanup_functions: list[
+    tuple[Callable[..., None], tuple[object, ...], dict[str, object]]
+] = []
 
 
-def register_atexit_func(func, *args, **kwargs):
-    # Register with atexit
+def _register_atexit_func(
+    func: Callable[..., None], *args: object, **kwargs: object
+) -> None:
     atexit.register(func, *args, **kwargs)
     # Also keep track of the cleanup functions for signal handling
     cleanup_functions.append((func, args, kwargs))
 
 
-def witness_atexit_execution():
+def _witness_atexit_execution() -> None:
     print("this print was triggered by atexit module")
     logger.debug("this log entry was triggered by atexit module")
 
 
-def signal_module_cleanup():
-    atexit.unregister(witness_atexit_execution)  # script will exit using signal
+def _signal_module_cleanup() -> None:
+    atexit.unregister(_witness_atexit_execution)
     for func, args, kwargs in cleanup_functions:
-        try:
-            print(f"called clean up func: {func.__name__}")
-            logger.info(f"called clean up func: {func.__name__}")
-            func(*args, **kwargs)
-            atexit.unregister(func)
-        except Exception as e:
-            print(f"Exception during cleanup: {e}")
-            logger.error(f"Exception during cleanup: {e}")
+        print(f"called clean up func: {func.__name__}")
+        logger.debug(f"called clean up func: {func.__name__}")
+        func(*args, **kwargs)
+        atexit.unregister(func)
 
 
-def signal_handler(sig, _):
+def _signal_handler(sig: int, _: FrameType | None) -> None:
     print(f"Signal {sig} received, calling cleanup.")
     logger.info(f"Signal {sig} received, calling cleanup.")
-    signal_module_cleanup()
+    _signal_module_cleanup()
     sys.exit(0)
 
 
-def setup_signal_handlers():
+def _setup_signal_handlers() -> None:
     logger.debug("Signal handlers are set up.")
-    signal.signal(signal.SIGTERM, signal_handler)
-    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, _signal_handler)
+    signal.signal(signal.SIGINT, _signal_handler)
 
 
-async def manage_script_startup(
+async def _manage_script_startup(
     slots_db_conn: aiosqlite.Connection,
     window_type: WinType,
     script_name: str,
-    lock_file_manager: Optional[LockFileManager] = None,
+    lock_file_manager: LockFileManager | None = None,
     secondary_windows: list[SecondaryWindow] | None = None,
 ) -> int | None:
-    setup_signal_handlers()
-    atexit.register(witness_atexit_execution)  # Lets us tell if cleanup
-    # function were called from the atexit module, or from signal.
-
     twm = TerminalWindowManager()
 
     slot, name = await twm.adjust_window(
         slots_db_conn, window_type, script_name, secondary_windows
     )
     if window_type == WinType.DENIED:
-        register_atexit_func(sdh.free_denied_slot_sync, slot)
+        _register_atexit_func(sdh.free_denied_slot_sync, slot)
         print(f"\n>>> Lock file is present for {script_name} <<<")
         logger.info(f"Lock file is present for {script_name}")
 
     elif window_type == WinType.ACCEPTED:
         if lock_file_manager:
             lock_file_manager.create_lock_file()
-            register_atexit_func(lock_file_manager.remove_lock_file)
-        register_atexit_func(sdh.free_slot_by_name_sync, name)
+            _register_atexit_func(lock_file_manager.remove_lock_file)
+        _register_atexit_func(sdh.free_slot_by_name_sync, name)
     return slot
 
 
@@ -93,21 +93,23 @@ async def setup_script(
     script_name: str,
     secondary_windows: list[SecondaryWindow] | None = None,
 ) -> tuple[aiosqlite.Connection, int | None]:
+    """Initialize the must have components for a terminal window managed script."""
     lock_file_manager = LockFileManager(script_name)
     db_conn = await sdh.create_connection(TERMINAL_WINDOW_SLOTS_DB_FILE_PATH)
 
     if not db_conn:
-        raise ValueError("Failed to create connection to the slots DB.")
+        e = f"Failed to connect to database: {TERMINAL_WINDOW_SLOTS_DB_FILE_PATH}"
+        raise ConnectionError(e)
 
-    setup_signal_handlers()
-    atexit.register(witness_atexit_execution)
+    _setup_signal_handlers()
+    atexit.register(_witness_atexit_execution)
 
     if lock_file_manager.lock_exists():
         window_type = WinType.DENIED
     else:
         window_type = WinType.ACCEPTED
 
-    slot = await manage_script_startup(
+    slot = await _manage_script_startup(
         db_conn, window_type, script_name, lock_file_manager, secondary_windows
     )
 

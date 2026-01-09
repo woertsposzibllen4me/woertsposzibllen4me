84a704f2e5cacee879246fa88d912180491ec659
Loïc Villet <villet.loic@gmail.com>
2026-01-09 11:55:50 +0100

    refactor(pregamespy): Make linters happy with tabbed.py

--- src/apps/pregamespy/core/tabbed.py
+++ src/apps/pregamespy/core/tabbed.py
@@ -1,61 +1,63 @@
+"""Module for managing mutually exclusive tabbed states."""
+
+from typing import final
+
+
+@final
 class Tabbed:
-    def __init__(self):
+    """Class to manage mutually exclusive tabbed states."""
+
+    def __init__(self) -> None:
+        """Initialize all states to False."""
         self._to_desktop = False
         self._to_dota_menu = False
         self._to_settings_screen = False
         self._in_game = False
 
     @property
-    def to_desktop(self):
+    def to_desktop(self) -> bool:
+        """Tabbed out to Windows desktop."""
         return self._to_desktop
 
     @to_desktop.setter
-    def to_desktop(self, value):
+    def to_desktop(self, value: bool) -> None:
         if value:
             self._set_all_false()
         self._to_desktop = value
 
     @property
-    def to_dota_menu(self):
+    def to_dota_menu(self) -> bool:
+        """Tabbed out to Dota 2 game menus (armory, heroes, etc.)."""
         return self._to_dota_menu
 
     @to_dota_menu.setter
-    def to_dota_menu(self, value):
+    def to_dota_menu(self, value: bool) -> None:
         if value:
             self._set_all_false()
         self._to_dota_menu = value
 
     @property
-    def to_settings_screen(self):
+    def to_settings_screen(self) -> bool:
+        """In the Dota2 floating window settings screen."""
         return self._to_settings_screen
 
     @to_settings_screen.setter
-    def to_settings_screen(self, value):
+    def to_settings_screen(self, value: bool) -> None:
         if value:
             self._set_all_false()
         self._to_settings_screen = value
 
     @property
-    def in_game(self):
+    def in_game(self) -> bool:
+        """In a live game."""
         return self._in_game
 
     @in_game.setter
-    def in_game(self, value):
+    def in_game(self, value: bool) -> None:
         if value:
             self._set_all_false()
         self._in_game = value
 
-    def _set_all_false(self):
+    def _set_all_false(self) -> None:
         for attr in self.__dict__:
-            if isinstance(self.__dict__[attr], bool):
-                self.__dict__[attr] = False
-
-    def current_state(self):
-        if self._to_desktop:
-            return "Out to desktop"
-        elif self._to_dota_menu:
-            return "In Dota menu"
-        elif self._to_settings_screen:
-            return "In settings screen"
-        else:
-            return "No state is True"
+            self.__dict__[attr] = False

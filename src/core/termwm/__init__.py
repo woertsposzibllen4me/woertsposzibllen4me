a1163609c5c9e8849eb11111d90422255cd7c6bb
Loïc Villet <villet.loic@gmail.com>
2026-01-11 23:27:45 +0100

    style(termw): Cut short unecessary docstring

--- src/core/termwm/__init__.py
+++ src/core/termwm/__init__.py
@@ -1,7 +1,4 @@
-"""
-Module for managing terminal windows. Usage is to create an instance of
-TerminalWindowManager and use its methods to adjust the windows of the script.
-"""
+"""Module for managing terminal windows."""
 
 from .core.constants import TERMINAL_WINDOW_SLOTS_DB_FILE_PATH
 from .core.twm_main import TerminalWindowManager
